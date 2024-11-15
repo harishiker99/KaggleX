bae26b6b3531859bd94563f6a730a83970ae3dd3
harishiker99 <nairharish1999@gmail.com>
2024-11-14 20:31:24 -0500

    fix: quantize.py with q8_0

--- quantize.py
+++ quantize.py
@@ -4,8 +4,8 @@ import subprocess
 command = [
     "python", "llama.cpp/convert_hf_to_gguf.py", 
     "Gemma-medtr-2b-sft-v2-hf", 
-    "--outfile", "Gemma-medtr-2b-sft.gguf", 
-    "--outtype", "bf16"
+    "--outfile", "Gemma-medtr-2b-sft-v2.gguf", 
+    "--outtype", "q8_0"
 ]
 
 # Run the command
